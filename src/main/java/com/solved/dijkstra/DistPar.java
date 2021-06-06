1a3a21f066e613dce73126c71de99da435ad0f4b
coolluck7 <coolluck7@gmail.com>
2021-06-06 21:57:13 +0300

    code with bugs =)

---
+++ src/main/java/com/solved/dijkstra/DistPar.java
@@ -0,0 +1,13 @@
+package com.solved.dijkstra;
+
+class DistPar // Расстояние и родительская вершина
+{ // Объекты сохраняются в массиве sPath
+    public int parentVert; // Текущий родитель вершины
+    public int distance; // Расстояние от начальной вершины до текущей
+
+    public DistPar(int parentVert, int distance) // Конструктор
+    {
+        this.parentVert = parentVert;
+        this.distance = distance;
+    }
+}
---
+++ src/main/java/com/solved/dijkstra/Vertex.java
@@ -0,0 +1,12 @@
+package com.solved.dijkstra;
+
+class Vertex {
+    public String name; // Метка (например, 'A')
+    public boolean isInTree;
+
+    public Vertex(String label) // Конструктор
+    {
+        this.name = label;
+        isInTree = false;
+    }
+}
